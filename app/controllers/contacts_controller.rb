c9a1a6b1ef7c52101c03880b8ba86aeee2f74207
Thomas Guaetta <ThomasGuaetta@gmail.com>
2022-11-01 16:54:25 +0000

    Styled Flash Messages, Moved Flash to application

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,22 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
   
+  # def create 
+  #   @contact = Contact.new(contact_params)
+  #   if @contact.save
+  #     redirect_to new_contact_path, notice: "Message Sent."
+  #   else
+  #     redirect_to new_contact_path, notice: "Error Occured."
+  #   end
+  # end
+  
   def create 
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Message Sent."
       redirect_to new_contact_path
     else
-      flash[:error] = @contact.errors.full_messages.join(", ")
+      flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end
   end
