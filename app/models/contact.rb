8f24f73e5303b230347be896e639e3263fa33345
Thomas Guaetta <ThomasGuaetta@gmail.com>
2022-11-01 19:06:58 +0000

    Added custom URL for Contact-Us

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,11 +3,6 @@ class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :comments, presence: true
-  
-  # Checks that the email field is a valid email
-  # validates :email,
-  # format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
-  #           uniqueness: { case_sensitive: false },
-  #           length: { minimum: 4, maximum: 254 }
+
             
 end
