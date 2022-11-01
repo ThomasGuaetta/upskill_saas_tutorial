58f0f6610fee674ab969d700f49576665570e44b
Thomas Guaetta <ThomasGuaetta@gmail.com>
2022-11-01 19:35:10 +0000

    Cleaned up routes and completed custom URL for Contact-us

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,15 +4,6 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
   
-  # def create 
-  #   @contact = Contact.new(contact_params)
-  #   if @contact.save
-  #     redirect_to new_contact_path, notice: "Message Sent."
-  #   else
-  #     redirect_to new_contact_path, notice: "Error Occured."
-  #   end
-  # end
-  
   def create 
     @contact = Contact.new(contact_params)
     if @contact.save
