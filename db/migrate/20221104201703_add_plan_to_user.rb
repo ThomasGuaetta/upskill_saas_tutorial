2c3d467ba8f12b60558323ff2f3045dee8fe1634
Thomas Guaetta <ThomasGuaetta@gmail.com>
2022-11-12 02:33:08 +0000

    Re-enabled Login

--- db/migrate/20221104201703_add_plan_to_user.rb
+++ db/migrate/20221104201703_add_plan_to_user.rb
@@ -1,5 +1,5 @@
 class AddPlanToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :plan_id, :integer
-  end
+  # def change
+  #   add_column :users, :plan_id, :integer
+  # end
 end
